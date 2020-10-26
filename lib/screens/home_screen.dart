a6206c341caa640f80cc3bda3fbd7cfc9444ab5b
sungyun-kim <niforances@gmail.com>
2020-10-26 23:59:05 +0900

    [Update] .jpg files
    my cat leon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,28 +21,35 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Image.asset(
-                  'images/cookie.png',
-                  fit: BoxFit.contain,
-                  height: 25,
-                ),
-                Container(
-                  padding: EdgeInsets.only(right: 1),
-                  child: Text('◁       인기순       ▶'),
-                ),
-                Icon(Icons.search),
-              ],
-            ),
-
-            //image
-            DefaultTabController(length: 9, child: WeeklyBar())
-          ],
-        ));
+      child: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              Image.asset(
+                'images/mycat.jpg',
+                fit: BoxFit.contain,
+                alignment: Alignment.center,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Image.asset(
+                    'images/cookie.png',
+                    fit: BoxFit.contain,
+                    height: 25,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(right: 1),
+                    child: Text('◀       인기순       ▶',style: TextStyle(color: Colors.black,fontSize: 11),
+                  ),
+                  Icon(Icons.search),
+                ],
+              ),
+            ],
+          ),
+          DefaultTabController(length: 9, child: WeeklyBar())
+        ],
+      ),
+    );
   }
 }
