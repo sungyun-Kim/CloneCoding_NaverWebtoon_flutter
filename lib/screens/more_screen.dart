7328d767d2d6b369ac5c1c6eb97d4151b99f70d9
sungyun-kim <niforances@gmail.com>
2020-10-31 23:39:35 +0900

    [Create] more_screen.dart

---
+++ lib/screens/more_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoreScreen extends StatefulWidget {
+  @override
+  _MoreScreenState createState() => _MoreScreenState();
+}
+
+class _MoreScreenState extends State<MoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
