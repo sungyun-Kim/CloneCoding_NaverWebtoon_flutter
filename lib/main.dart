651b30ec07ecc1a28caabc3c552beafefa43e59e
Niforances <niforances@gmail.com>
2020-11-01 14:23:26 +0900

    [Update] main.dart
    fixed to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:clonecoding_naverwebtoon/screens/more_screen.dart';
 import 'package:clonecoding_naverwebtoon/widgets/bottom_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
@@ -15,10 +16,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //no physics
       title: 'ksy',
+
       home: DefaultTabController(
         length: 5,
         child: Scaffold(
