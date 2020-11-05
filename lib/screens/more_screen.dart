402e05bec2868a7c5d9ab0e22fe565b5df914f1a
sungyun-kim <niforances@gmail.com>
2020-11-05 09:32:00 +0900

    [Update] more_screen.dart
    add comments

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -5,6 +5,12 @@ class MoreScreen extends StatefulWidget {
   _MoreScreenState createState() => _MoreScreenState();
 }
 
+//title: 더보기
+//userName 님 (Text)
+//로그인계정설정 > (InkWell)
+//내 쿠키  충전하기 > btn or InkWell
+//InkWell with Icons : GridView { 검색, 쿠키오븐, 겟짤, PLAY, 공지사항, 설정 }
+
 class _MoreScreenState extends State<MoreScreen> {
   @override
   Widget build(BuildContext context) {
